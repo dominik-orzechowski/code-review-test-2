a2115b1785b631b1a6938026efec50e0d9b6fd3e
Dominik <dominik.orzechowski@ordergroup.pl>
2024-06-24 09:53:42 +0200

    test-6

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
+  const name = 'Test1';
+  const surname = 'test2';
+  const handleSendApiRequet = (arg) => {
+    console.log(name, surname);
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button onClick={() => handleSendApiRequet()}></button>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
